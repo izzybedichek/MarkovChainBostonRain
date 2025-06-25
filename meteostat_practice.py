aac053d492c8aad4e8f60661415fb7164485d94e
IzzyBedic <izzy@bedichek.org>
2025-06-25 12:46:39 -0400

    getting precipitation data, cleaning it, and putting it into a pandas dataframe

--- meteostat_practice.py
+++ meteostat_practice.py
@@ -6,8 +6,22 @@ from meteostat import Point, Daily
 # https://en.wikipedia.org/wiki/Boston
 boston = Point( 42.3736, -71.0861, 14)
 
+# setting the time to be approximately a calendar year
 start_date = datetime(2024, 6, 24)
 end_date = datetime(2025, 6, 24)
 
+# getting daily weather data
 boston_weather = Daily(boston, start_date, end_date)
 boston_weather = boston_weather.fetch()
+
+# copying the data frame for best practice data handling
+rain_data = boston_weather.copy()
+
+# checking the output
+print(rain_data)
+
+# taking only precipitation and datetime, resetting index so that datetime is considered a column rather than an index
+rain_data = rain_data[["prcp"]].reset_index()
+
+# checking the output
+print(rain_data)
