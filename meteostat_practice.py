b7e7b79eff6c1dc0e744af02dd60cd0189d21cd6
IzzyBedic <izzy@bedichek.org>
2025-06-25 12:39:12 -0400

    datetime

--- meteostat_practice.py
+++ meteostat_practice.py
@@ -1,4 +1,5 @@
 # pip install meteostat in terminal
+from datetime import datetime
 from meteostat import Point, Daily
 
 # source for lat, long, altitude (in meters): https://wiki.openstreetmap.org/wiki/Boston,_Massachusetts#:~:text=Boston%20is%20a%20city%20in,°05′09.96″%20West.
@@ -8,5 +9,5 @@ boston = Point( 42.3736, -71.0861, 14)
 start_date = datetime(2024, 6, 24)
 end_date = datetime(2025, 6, 24)
 
-boston_weather = Daily(boston, start, end)
+boston_weather = Daily(boston, start_date, end_date)
 boston_weather = boston_weather.fetch()
