3486ed930ef0a339af6a55506b68516edbf48639
IzzyBedic <izzy@bedichek.org>
2025-07-20 13:56:57 -0400

    adding cases

--- analysis.py
+++ analysis.py
@@ -16,8 +16,15 @@ initial_state_clear = np.array([0,1]).reshape(2,1)
 # 10/90 chance = .9, .1.
 initial_state_nine_to_one = np.array([.9,.1]).reshape(2,1)
 
+# 50/950 chance = .5, .5.
+initial_state_half = np.array([.5,.5]).reshape(2,1)
+
 one_week_after = df*initial_state_nine_to_one
 two_weeks_after = (df**2)*initial_state_nine_to_one
 
 print("one week: \n", one_week_after, ", \n", "two weeks: \n", two_weeks_after)
 
+one_week_after = df*initial_state_half
+two_weeks_after = (df**2)*initial_state_half
+
+print("one week: \n", one_week_after, ", \n", "two weeks: \n", two_weeks_after)
