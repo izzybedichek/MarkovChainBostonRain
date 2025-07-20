8c0038576d12c4b5580065cf3a81d74114adc78d
IzzyBedic <izzy@bedichek.org>
2025-07-20 13:52:34 -0400

    made the initial state 10% chance of rain based off of today's weather forecast

--- analysis.py
+++ analysis.py
@@ -7,11 +7,17 @@ print(matrix)
 
 df = pd.DataFrame(matrix, columns=['rain', 'clear'], index=['rain', 'clear'])
 
+# must rain on first day = 1,0. 50/50 chance = .5, .5
 initial_state_rain = np.array([1,0]).reshape(2,1)
+
+# must NOT rain on first day = 0,1. 50/50 chance = .5, .5.
 initial_state_clear = np.array([0,1]).reshape(2,1)
 
-one_week_after_rain = df*initial_state_rain
-one_week_after_clear = df*initial_state_clear
+# 10/90 chance = .9, .1.
+initial_state_nine_to_one = np.array([.9,.1]).reshape(2,1)
 
-print("rain: \n", one_week_after_rain, ", \n", "clear: \n", one_week_after_clear)
+one_week_after = df*initial_state_nine_to_one
+two_weeks_after = (df**2)*initial_state_nine_to_one
+
+print("one week: \n", one_week_after, ", \n", "two weeks: \n", two_weeks_after)
 
