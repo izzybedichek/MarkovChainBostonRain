977cdf025e0d0a4ab86eb8c547a113df344b1c2f
IzzyBedic <izzy@bedichek.org>
2025-07-20 13:25:03 -0400

    converting to df w column and index labels for clarity

--- analysis.py
+++ analysis.py
@@ -5,13 +5,13 @@ import numpy as np
 from transition_matrix import *
 
 matrix = weekend_only.copy()
-
+df = pd.DataFrame(matrix, columns=['rain', 'clear'], index=['rain', 'clear'])
 
 initial_state_rain = np.array([1,0])
 initial_state_clear = np.array([0,1])
 
-one_week_after_rain = matrix*initial_state_rain
-one_week_after_clear = matrix*initial_state_clear
+one_week_after_rain = df*initial_state_rain
+one_week_after_clear = df*initial_state_clear
 
 print("rain: ", one_week_after_rain, ",", "clear: ", one_week_after_clear)
 
