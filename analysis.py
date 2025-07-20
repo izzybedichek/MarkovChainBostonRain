aeec3b7792b4e2a232fcf395a36af02e74083542
IzzyBedic <izzy@bedichek.org>
2025-07-20 13:36:00 -0400

    reshaping the initial states to be accurate

--- analysis.py
+++ analysis.py
@@ -5,13 +5,14 @@ import numpy as np
 from transition_matrix import *
 
 matrix = weekend_only.copy()
+
 df = pd.DataFrame(matrix, columns=['rain', 'clear'], index=['rain', 'clear'])
 
-initial_state_rain = np.array([1,0])
-initial_state_clear = np.array([0,1])
+initial_state_rain = np.array([1,0]).reshape(2,1)
+initial_state_clear = np.array([0,1]).reshape(2,1)
 
 one_week_after_rain = df*initial_state_rain
 one_week_after_clear = df*initial_state_clear
 
-print("rain: ", one_week_after_rain, ",", "clear: ", one_week_after_clear)
+print("rain: ", one_week_after_rain, ", \n", "clear: ", one_week_after_clear)
 
