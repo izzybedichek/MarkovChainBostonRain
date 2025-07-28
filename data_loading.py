7c354f52acd9aaca37faf5530d8b53edad630972
IzzyBedic <izzy@bedichek.org>
2025-07-28 15:26:20 -0400

    sorted files

--- data_loading.py
+++ data_loading.py
@@ -10,6 +10,9 @@ boston = Point( 42.3736, -71.0861, 14)
 start_date = datetime(2024, 6, 24)
 end_date = datetime(2025, 6, 24)
 
+#start_date = datetime(2020, 6, 24)
+#end_date = datetime(2025, 6, 24)
+
 # getting daily weather data
 boston_weather = Daily(boston, start_date, end_date)
 boston_weather = boston_weather.fetch()
