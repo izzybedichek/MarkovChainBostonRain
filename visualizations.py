089f6c8a7b96037582a48f5d7ddce214c4457ccb
IzzyBedic <izzy@bedichek.org>
2025-07-29 14:47:04 -0400

    creating visualizations from the matrices

--- visualizations.py
+++ visualizations.py
@@ -1,23 +1,27 @@
 # NEED TO GET DISPLAY
+import pandas as pd
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
 # https://stackoverflow.com/questions/22785849/drawing-multiple-edges-between-two-nodes-with-networkx
 
+data = pd.read_csv('csv/transition_matrix.csv')
+print(data)
+
 def my_draw_networkx_edge_labels(
     G,
     pos,
     edge_labels=None,
     label_pos=0.5,
-    font_size=10,
+    font_size=20,
     font_color="k",
     font_family="sans-serif",
     font_weight="normal",
     alpha=None,
     bbox=None,
     horizontalalignment="center",
-    verticalalignment="center",
+    verticalalignment="bottom",
     ax=None,
     rotate=True,
     clip_on=True,
@@ -129,6 +133,7 @@ def my_draw_networkx_edge_labels(
                 angle -= 180
             if angle < -90:
                 angle += 180
+
             # transform data coordinate angle to screen coordinate angle
             xy = np.array((x, y))
             trans_angle = ax.transData.transform_angles(
@@ -136,6 +141,7 @@ def my_draw_networkx_edge_labels(
             )[0]
         else:
             trans_angle = 0.0
+
         # use default box of white with white border
         if bbox is None:
             bbox = dict(boxstyle="round", ec=(1.0, 1.0, 1.0), fc=(1.0, 1.0, 1.0))
@@ -173,15 +179,15 @@ def my_draw_networkx_edge_labels(
     return text_items
 
 G = nx.DiGraph()
-edge_list = [("rain", "rain", {'w': '0.926829'}), ("clear", "clear", {'w': '0.073171'}),
-             ("rain", "clear", {'w': '0.222222'}), ("clear", "rain", {'w': '0.777778'})]
+edge_list = [("rain", "rain", {'w': '0.600000'}), ("clear", "clear", {'w': '0.428571'}),
+             ("rain", "clear", {'w': '0.400000'}), ("clear", "rain", {'w': '0.571429'})]
 
 G.add_nodes_from(["rain", "clear"])
 G.add_edges_from(edge_list)
 pos = nx.spring_layout(G, seed=5)
-fig, ax = plt.subplots()
-nx.draw_networkx_nodes(G, pos, ax=ax)
-nx.draw_networkx_labels(G, pos, ax=ax)
+fig, ax = plt.subplots(figsize=(15, 12))
+nx.draw_networkx_nodes(G, pos, node_color="turquoise", ax=ax)
+nx.draw_networkx_labels(G, pos, font_size = 18, ax=ax)
 
 curved_edges = [edge for edge in G.edges() if reversed(edge) in G.edges()]
 straight_edges = list(set(G.edges()) - set(curved_edges))
@@ -194,10 +200,74 @@ nx.draw_networkx_edges(G, pos, ax=ax, edgelist=curved_edges,
 edge_weights = nx.get_edge_attributes(G, 'w')
 curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
 straight_edge_labels = {edge: edge_weights[edge] for edge in straight_edges}
-my_draw_networkx_edge_labels(G, pos, ax=ax,
+my_draw_networkx_edge_labels(G, pos, ax=ax, font_size = 20,
                                    edge_labels=curved_edge_labels, rotate=False,
                                    rad=arc_rad)
 nx.draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=straight_edge_labels,
                              rotate=False)
 
-nx.draw(G)
+
+plt.show()
+
+H = nx.DiGraph()
+edge_list = [("No rain", "No rain", {'w': '0.42857'}), ("No rain", "Only Friday", {'w': '0.04762'}),
+             ("No rain", "Only Saturday", {'w': '0.47619'}), ("No rain", "Only Sunday", {'w': '0.142857'}),
+             ("No rain", "Friday and Saturday", {'w': '0.142857'}), ("No rain", "Friday and Sunday", {'w': '0.095238'}),
+             ("No rain", "Saturday and Sunday", {'w': '0.0'}), ("No rain", "Rain all weekend", {'w': '0.095238'}),
+             ("Only Friday", "No rain", {'w': '0.0'}), ("Only Friday", "Only Friday", {'w': '0.0'}),
+             ("Only Friday", "Only Saturday", {'w': '0.0'}), ("Only Friday", "Only Sunday", {'w': '0.0'}),
+             ("Only Friday", "Friday and Saturday", {'w': '0.33333'}), ("Only Friday", "Friday and Sunday", {'w': '0.0'}),
+             ("Only Friday", "Saturday and Sunday", {'w': '0.66667'}), ("Only Friday", "Rain all weekend", {'w': '0.0'}),
+             ("Only Saturday", "No rain", {'w': '0.42857'}), ("Only Saturday", "Only Friday", {'w': '0.04762'}),
+             ("Only Saturday", "Only Saturday", {'w': '0.5'}), ("Only Saturday", "Only Sunday", {'w': '0.25'}),
+             ("Only Saturday", "Friday and Saturday", {'w': '0.0'}), ("Only Saturday", "Friday and Sunday", {'w': '0.0'}),
+             ("Only Saturday", "Saturday and Sunday", {'w': '0.25'}), ("Only Saturday", "Rain all weekend", {'w': '0.0'}),
+             ("Only Sunday", "No rain", {'w': '0.6'}), ("Only Sunday", "Only Friday", {'w': '0.4'}),
+             ("Only Sunday", "Only Saturday", {'w': '0.0'}), ("Only Sunday", "Only Sunday", {'w': '0.0'}),
+             ("Only Sunday", "Friday and Saturday", {'w': '0.0'}), ("Only Sunday", "Friday and Sunday", {'w': '0.0'}),
+             ("Only Sunday", "Saturday and Sunday", {'w': '0.0'}), ("Only Sunday", "Rain all weekend", {'w': '0.0'}),
+             ("Friday and Saturday", "No rain", {'w': '0.5'}), ("Friday and Saturday", "Only Friday", {'w': '0.0'}),
+             ("Friday and Saturday", "Only Saturday", {'w': '0.16667'}), ("Friday and Saturday", "Only Sunday", {'w':'0.16667'}),
+             ("Friday and Saturday", "Friday and Saturday", {'w': '0.0'}), ("Friday and Saturday", "Friday and Sunday", {'w': '0.0'}),
+             ("Friday and Saturday", "Saturday and Sunday", {'w': '0.0'}), ("Friday and Saturday", "Rain all weekend", {'w': '0.16667'}),
+             ("Friday and Sunday", "No rain", {'w': '0.5'}), ("Friday and Sunday", "Only Friday", {'w': '0.0'}),
+             ("Friday and Sunday", "Only Saturday", {'w': '0.0'}), ("Friday and Sunday", "Only Sunday", {'w': '0.0'}),
+             ("Friday and Sunday", "Friday and Saturday", {'w': '0.5'}), ("Friday and Sunday", "Friday and Sunday", {'w': '0.0'}),
+             ("Friday and Sunday", "Saturday and Sunday", {'w': '0.0'}), ("Friday and Sunday", "Rain all weekend", {'w': '0.0'}),
+             ("Saturday and Sunday", "No rain", {'w': '0.6'}), ("Saturday and Sunday", "Only Friday", {'w': '0.0'}),
+             ("Saturday and Sunday", "Only Saturday", {'w': '0.0'}), ("Saturday and Sunday", "Only Sunday", {'w': '0.0'}),
+             ("Saturday and Sunday", "Friday and Saturday", {'w': '0.0'}), ("Saturday and Sunday", "Friday and Sunday", {'w': '0.0'}),
+             ("Saturday and Sunday", "Saturday and Sunday", {'w': '0.2'}), ("Saturday and Sunday", "Rain all weekend", {'w': '0.2'}),
+             ("Rain all weekend", "No rain", {'w': '0.4'}), ("Rain all weekend", "Only Friday", {'w': '0.0'}),
+             ("Rain all weekend", "Only Saturday", {'w': '0.0'}), ("Rain all weekend", "Only Sunday", {'w': '0.0'}),
+             ("Rain all weekend", "Friday and Saturday", {'w': '0.2'}), ("Rain all weekend", "Friday and Sunday", {'w': '0.0'}),
+             ("Rain all weekend", "Saturday and Sunday", {'w': '0.2'}), ("Rain all weekend", "Rain all weekend", {'w': '0.2'})
+             ]
+
+H.add_nodes_from(["No rain", "Only Friday", "Only Saturday", "Only Sunday", "Friday and Saturday", "Friday and Sunday",
+                  "Saturday and Sunday", "Rain all weekend"])
+H.add_edges_from(edge_list)
+pos = nx.spring_layout(H, seed=5)
+fig, ax = plt.subplots(figsize=(30, 30))
+nx.draw_networkx_nodes(H, pos, node_color="turquoise", ax=ax)
+nx.draw_networkx_labels(H, pos, font_size = 18, ax=ax)
+
+curved_edges = [edge for edge in H.edges() if reversed(edge) in H.edges()]
+straight_edges = list(set(H.edges()) - set(curved_edges))
+nx.draw_networkx_edges(H, pos, ax=ax, edgelist=straight_edges)
+arc_rad = 0.25
+nx.draw_networkx_edges(H, pos, ax=ax, edgelist=curved_edges,
+                       connectionstyle=f'arc3, rad = {arc_rad}')
+
+
+edge_weights = nx.get_edge_attributes(H, 'w')
+curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
+straight_edge_labels = {edge: edge_weights[edge] for edge in straight_edges}
+my_draw_networkx_edge_labels(G, pos, ax=ax, font_size = 20,
+                                   edge_labels=curved_edge_labels, rotate=False,
+                                   rad=arc_rad)
+nx.draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=straight_edge_labels,
+                             rotate=False)
+
+
+plt.show()
